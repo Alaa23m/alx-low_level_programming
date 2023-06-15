d043fd99612ff4f996173ba42e6f544ce24f090e
Alaa23m <yiu@example.com>
2023-06-15 16:14:38 -0700

    oo

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -Entry 
+ * 
+ * Description: it's a program that prints with put function.
+ *
+ * Return: Alawys 0 (Success)
+*/
+
+int main(void) 
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
