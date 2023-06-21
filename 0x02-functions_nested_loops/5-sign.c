f75a93f3501ea497c0f9d5c80b661650c4c856aa
Alaa23m <yiu@example.com>
2023-06-21 13:43:38 -0700

    ooo

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,38 +7,21 @@
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
  */
-
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-        printf("+");
-        return 1;
-    }
-    else if (n == 0)
-    {
-        printf("0");
-        return 0;
-    }
-    else
-    {
-        printf("-");
-        return -1;
-    }
-}
-
-int main(void)
-{
-    int r;
-
-    r = print_sign(98);
-    printf(", %d\n", r);
-    r = print_sign(0);
-    printf(", %d\n", r);
-    r = print_sign(0xff);
-    printf(", %d\n", r);
-    r = print_sign(-1);
-    printf(", %d\n", r);
-
-    return 0;
+	if (n > 0)
+	{
+		putchar('+');
+		return 1;
+	}
+	else if (n == 0)
+	{
+		putchar('0');
+		return 0;
+	}
+	else
+	{
+		putchar('-');
+		return -1;
+	}
 }
