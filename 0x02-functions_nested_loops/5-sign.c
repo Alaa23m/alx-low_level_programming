ec56ea90adbf2974f82024f38a19a5aaaf67d275
Alaa23m <yiu@example.com>
2023-06-21 13:36:45 -0700

    ooo

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
  * print_sign - prints the sign of a number
@@ -15,7 +15,7 @@ int print_sign(int n)
 		return (1);
 	} else if (n == 0)
 	{
-		_putchar(48);
+		_putchar('0');
 		return (0);
 	} else if (n < 0)
 	{
